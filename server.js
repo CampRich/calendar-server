cb0798dddd3e308a882ef54da407ef8120f25054
CampRich <apond82@gmail.com>
2025-07-12 08:43:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -43,4 +43,5 @@ app.delete('/events', async (req, res) => {
   res.json({ message: 'Events deleted' });
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
